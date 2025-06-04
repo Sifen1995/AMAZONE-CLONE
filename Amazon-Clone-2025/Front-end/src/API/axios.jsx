f5c5520d75d635233c4fdfc285ad20638022e971
sifen getachew <sifenmesfin10@gmail.com>
2025-06-03 19:11:12 -0700

    backend url updated

--- Amazon-Clone-2025/Front-end/src/API/axios.jsx
+++ Amazon-Clone-2025/Front-end/src/API/axios.jsx
@@ -1,7 +1,11 @@
 import axios from 'axios'
 
  const axiosInstance=axios.create({
-    baseURL:"http://localhost:5000"
+   // baseURL:"http://localhost:5000"
+   
+   //deployed on render
+   baseURL:"https://amazone-clone-backend-k4mr.onrender.com"
+    
 })
 
 export {axiosInstance};
