cde0ef4a6d29468f4f92c94f37e0948e074d5c40
sifen getachew <sifenmesfin10@gmail.com>
2025-06-04 02:53:54 -0700

    order page edited

--- Amazon-Clone-2025/Front-end/src/pages/orders/Orders.jsx
+++ Amazon-Clone-2025/Front-end/src/pages/orders/Orders.jsx
@@ -13,57 +13,124 @@ import {
 
 import ProductCard from '../../components/product/ProductCard';
 
-export default function Orders() {
-  const { user } = useContext(DataContext);
-  const [orders, setOrders] = useState([]);
+// export default function Orders() {
+//   const { user } = useContext(DataContext);
+//   const [orders, setOrders] = useState([]);
 
+//   useEffect(() => {
+//     if (!user) {
+//       setOrders([]);
+//       return;
+//     }
+
+//     const ordersRef = collection(db, 'users', user.uid, 'orders');
+//     const q = query(ordersRef, orderBy('created', 'desc'));
+
+//     const unsubscribe = onSnapshot(q, (snapshot) => {
+//       const updatedOrders = snapshot.docs.map((doc) => ({
+//         id: doc.id,
+//         data: doc.data(),
+//       }));
+//       setOrders(updatedOrders);
+//     });
+
+//     return () => unsubscribe(); // Clean up on unmount
+//   }, []);
+
+//   return (
+//     <Layout>
+//       <section className={Class.container}>
+//         <div className={Class.order__container}>
+//           <h2>Your Orders</h2>
+//           <div>
+//             {orders.length === 0 && <p>You don't have orders yet.</p>}
+//             {orders.map((order,i) => (
+              
+//                 <div key={order.i}>
+//                   <hr />
+//                   <p>Order ID:{order?.id}</p>
+//                   {
+//                     order?.data.basket?.map((o)=>{
+//                       return  <ProductCard
+//                       flex={true}
+//                       item={o}
+//                       key={o.id}/>
+//                     })
+//                   }
+//                 </div>
+               
+             
+//             ))}
+//           </div>
+//         </div>
+//       </section>
+//     </Layout>
+//   );
+// }
+
+ export default function Orders() {
+  const { user, dispatch } = useContext(DataContext)
+  const [orders, setOrders] =  useState([])
+  
   useEffect(() => {
-    if (!user) {
-      setOrders([]);
-      return;
+    if (user) {
+    
+      const ordersRef = collection(doc(db, "users", user.uid), "orders");
+
+     
+      const ordersQuery = query(ordersRef, orderBy("created", "desc"));
+
+     
+      onSnapshot(ordersQuery, (snaps) => {
+        console.log( snaps);
+        
+        setOrders(snaps.docs.map((doc) => ({
+          id: doc.id,
+          data:doc.data()
+        })))
+      });
+    } else {
+      setOrders([])
     }
 
-    const ordersRef = collection(db, 'users', user.uid, 'orders');
-    const q = query(ordersRef, orderBy('created', 'desc'));
 
-    const unsubscribe = onSnapshot(q, (snapshot) => {
-      const updatedOrders = snapshot.docs.map((doc) => ({
-        id: doc.id,
-        data: doc.data(),
-      }));
-      setOrders(updatedOrders);
-    });
-
-    return () => unsubscribe(); // Clean up on unmount
-  }, []);
-
+  }, [])
   return (
     <Layout>
       <section className={Class.container}>
         <div className={Class.order__container}>
-          <h2>Your Orders</h2>
+          <h2> Your Orders</h2>
+          {
+            orders?.length==0 && <div style={{padding: "20px"}}> You don't have orders yet.</div>
+          }
           <div>
-            {orders.length === 0 && <p>You don't have orders yet.</p>}
-            {orders.map((order,i) => (
-              
-                <div key={order.i}>
-                  <hr />
-                  <p>Order ID:{order?.id}</p>
-                  {
-                    order?.data.basket?.map((o)=>{
-                      return  <ProductCard
-                      flex={true}
-                      item={o}
-                      key={o.id}/>
-                    })
-                  }
-                </div>
-               
-             
-            ))}
+            {
+              orders?.map((eachorders, i) => {
+                return (
+                  <div key={i}>
+                    <hr />
+                    <p>Order ID : {eachorders?.id}</p>
+                    {
+                      eachorders?.data.basket?.map(order => {
+                        return (
+                          <ProductCard
+                            flex={true}
+                            item={order}
+                            key={order.id}
+                          />
+                        );
+                      
+                      })
+                    }
+                  </div>
+                )
+              })
+            }
+
           </div>
         </div>
       </section>
     </Layout>
-  );
+  )
 }
+
